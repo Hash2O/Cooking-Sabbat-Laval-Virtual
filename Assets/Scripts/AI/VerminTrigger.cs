fbc70b50ca97e2a39a2274ddccc5c289f8182827
marie.leblanc0410@gmail.com <marie.leblanc0410@gmail.com>
2026-04-02 18:48:23 +0200

    upd: collider of static ingredient + change mvermin comportement

--- Assets/Scripts/AI/VerminTrigger.cs
+++ Assets/Scripts/AI/VerminTrigger.cs
@@ -1,28 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class VerminTrigger : MonoBehaviour
 {
 
-    public float speed;
+     [Header("References")]
 
-    private VerminMovement currentManager;
-    void Start()
+    [SerializeField]
+    private Animator animator;
+
+    [Header("Stats")]
+
+    [SerializeField]
+    private float walkSpeed;
+
+    [SerializeField]
+    private float rotationSpeed;
+
+
+
+    [Header("Wandering parameters")]
+
+    [SerializeField]
+    private List<Transform> pathPoints = new();
+
+    [SerializeField]
+    private Transform actualPoint;
+
+    private bool isDead = false;
+
+
+    public float switchDistance = 1.5f; 
+
+    private void Start() 
     {
+        animator = this.GetComponentInChildren<Animator>();  
+    }
+    void Update()
+    {
+        if (isDead) return;
+        if(pathPoints.Count == 0) return;
+        if(actualPoint == null)
+        {
+            actualPoint = pathPoints[Random.Range(0, pathPoints.Count)];
+        }
+
+        if (Vector3.Distance(this.transform.position, actualPoint.position) <= switchDistance)
+        {
+            int randomNum = Random.Range(0, pathPoints.Count);
+            actualPoint = pathPoints[randomNum];
+        }
+
+        Vector3 directionToTarget = actualPoint.position - this.transform.position;
+        directionToTarget.y = 0; // On garde ça plat
+
+        if (directionToTarget != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(directionToTarget);
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+        this.transform.position += this.transform.forward * walkSpeed * Time.deltaTime;
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void KillVermin()
     {
+        if (isDead) return;
+
+        isDead = true;
         
+        if (animator != null)
+        {
+            animator.SetTrigger("Death");
+            StartCoroutine(WaitForDeathAnimation());
+        }
+        else
+        {
+            DestroyVerminObject();
+        }
+    }
+
+    private IEnumerator WaitForDeathAnimation()
+    {
+        yield return null; 
+        yield return new WaitForSeconds(0.5f);
+
+        DestroyVerminObject();
+    }
+
+    private void DestroyVerminObject()
+    {
+        Destroy(this.gameObject);
+        isDead = true;
     }
 
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "SimiliNavMesh")
         {
-            currentManager = other.gameObject.GetComponent<VerminMovement>();
-            other.gameObject.GetComponent<VerminMovement>().VerminEnter(this.gameObject.transform, speed);
+            isDead = false;
+            pathPoints = other.gameObject.GetComponent<VerminMovement>().pathPoints;
+            this.transform.rotation = new Quaternion(0, this.transform.rotation.y, 0, 0);
+            if(animator != null)
+                animator.SetBool("Walk", true);
         }
     }
 
@@ -30,19 +111,15 @@ public class VerminTrigger : MonoBehaviour
     {
         if(other.gameObject.tag == "SimiliNavMesh")
         {
-            other.gameObject.GetComponent<VerminMovement>().VerminExit();
-            currentManager = null;
+            isDead = true;
+            if(animator != null)
+            {
+                animator.SetBool("Walk", false);   
+            }
         }
     }
     public void HitByWeapon()
     {
-        if (currentManager != null)
-        {
-            currentManager.KillVermin();
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
+        KillVermin();
     }
 }
